7cea1a876b1d81e57b12e3ab4f0b5fe5afdd991f
Senilka <senilka0108@gmail.com>
2024-01-29 12:25:54 +0530

    Welcome-note

---
+++ WelcomeNimal.py
@@ -0,0 +1,9 @@
+#take the user's name as input
+name = input("Enter your name: ")
+print(name)
+
+#the variable that includes the welcome message is already provided.
+#Please complete this part using the knowledge obtained in this lesson.
+
+#print the welcome message
+print("Welcome", name)
